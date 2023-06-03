e2eca7a2e9f992c9218a707bf5fa2d0cea584347
Severle <2096545154@qq.com>
2023-06-03 10:13:54 +0800

    Init

---
+++ src/main/java/org/severle/Main.java
@@ -0,0 +1,10 @@
+package org.severle;
+
+import org.severle.ui.MainForm;
+
+public class Main {
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.start();
+    }
+}
