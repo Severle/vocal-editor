ed0cf9a2a77390d794f7d022bbbba60860b59366
Severle <2096545154@qq.com>
2023-06-03 21:55:15 +0800

    Init

--- src/main/java/org/severle/text/Text.java
+++ src/main/java/org/severle/text/Text.java
@@ -1,5 +1,8 @@
 package org.severle.text;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Text {
     public static String translate(String key) {
         return Language.getInstance().getValue(key);
